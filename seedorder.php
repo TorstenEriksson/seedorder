dd13a2f550f72bb089c1f9c971ff0617d1ce0335
Torsten Eriksson <torsten@utsteg.se>
2026-02-03 16:43:29 +0100

    Second ordering period was not set correctly as ordering time

--- seedorder.php
+++ seedorder.php
@@ -17,7 +17,7 @@
     $my_template = 'seedorder.html.twig';
     $ordering_now = ($today >= $config->getSetting('ORG_ORDERING_START') && $today <= $config->getSetting('ORG_ORDERING_GRACE_DATE')) ? 1 : 0;
     if ($ordering_now == 0) {
-        ($today >= $config->getSetting('ORG_SECOND_START') && $today <= $config->getSetting('ORG_SECOND_END')) ? 2 : 0;
+        $ordering_now = ($today >= $config->getSetting('ORG_SECOND_START') && $today <= $config->getSetting('ORG_SECOND_END')) ? 2 : 0;
     }
     $template_data = [
         'page_title' => $config->getSetting('ORG_NAME'),
